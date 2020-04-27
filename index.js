cb6353e65ff0bd4c2463d5fdd1b9607bf5eb7f84
alivahidi78 <a_vahidi@outlook.com>
2020-04-27 21:03:35 +0430

    bootstrap & data

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 const PORT = 3000;
 
 app.listen(PORT, () => { console.log(`listening on Port ${PORT}`) });
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => { res.render('pages/home'); });
+
+app.get('/', (req, res) => {
+    axios.get('http://covid19api.xapix.io/v2/locations')
+        .then((response) => {
+            res.render('pages/home', { data: response.data });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
